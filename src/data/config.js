5987ad101e9d8ead00401681ce552958cef51471
CAIO VIEIRA <caio@dataclick.com.br>
2020-08-14 09:38:33 -0300

    feat: new config

--- src/data/config.js
+++ src/data/config.js
@@ -1,24 +1,24 @@
 module.exports = {
   defaultTitle: 'Caio Vieira',
-  logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
+  logo: 'https://www.caiovieira.com.br/favicon/favicon-512.png',
   author: 'Caio Vieira',
-  url: 'https://portfolio.smakosh.com',
+  url: 'https://www.caiovieira.com.br',
   legalName: 'Caio Vieira',
   defaultDescription: 'Sou Caio Vieira, desenvolvedor FullStack especialista em ReactJs e NodeJS',
   socialLinks: {
     twitter: 'http://www.twitter.com/cvieira850',
     github: 'https://github.com/cvieira850',
-    linkedin: 'https://www.linkedin.com/in/ismail-ghallou-630149122/',
+    linkedin: 'https://www.linkedin.com/in/caiodmvieira/',
     instagram: 'https://instagram.com/smakosh19',
-    youtube: 'https://www.youtube.com/user/smakoshthegamer',
-    google: 'https://plus.google.com/u/0/+IsmailSmakoshGhallou',
+    youtube: 'https://www.youtube.com/',
+    google: 'https://plus.google.com/u/0/+cvieira850',
   },
   googleAnalyticsID: 'UA-175195911-1',
   themeColor: '#6b63ff',
   backgroundColor: '#6b63ff',
   social: {
     facebook: 'appId',
-    twitter: '@smakosh',
+    twitter: '@cvieira850',
   },
   address: {
     city: 'City',
@@ -30,5 +30,5 @@ module.exports = {
     email: 'email',
     phone: 'phone number',
   },
-  foundingDate: '2018',
+  foundingDate: '2020',
 };
